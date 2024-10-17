d2ff7e4b2da91ce78bc3a0416b6e9d37350f098c
rayhanalmim <rayhanalmim1@gmail.com>
2024-10-17 18:15:02 +0600

    implement folder structure of the server and add some basic utils and route

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express, { Application, Request, Response } from "express";
+import { PrismaClient } from '@prisma/client';
+import cors from "cors";
+
+const app: Application = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(cors());
+const port = 3013;
+
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+  });
+  
+  // Not Found Route Handler
+  app.use((req: Request, res: Response) => {
+    res.status(404).json({
+      success: false,
+      message: "Route not found",
+    });
+  });
+  
+  app.use((err: any, req: Request, res: Response) => {
+    console.error(err.stack);
+    res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred",
+    });
+  });
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
